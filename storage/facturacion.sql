b6efe87be53e0a53155beca618e799d85e0f57f7
Edgard Decena <edecena@gmail.com>
2026-06-22 19:43:42 -0400

    [UPD] update facturacion.sql

--- storage/facturacion.sql
+++ storage/facturacion.sql
@@ -60,36 +60,50 @@ CREATE TABLE detalle_factura (
 	CONSTRAINT ch_detalle_precio CHECK (precio > 0)
 );
 
--- INSERTS INTO CLIENTE:
-INSERT INTO cliente (tipo_doc, num_doc, nombre)
-	VALUES	('V', '123', 'Pedro'),
-			('V', '456', 'Maria'),
-			('V', '789', 'Juan');
+-- 1. Tabla: cliente
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('V', '12345678', 'Juan Perez');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('E', '87654321', 'Maria Garcia');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('J', '10203040', 'Empresa Polar');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('V', '11223344', 'Carlos Lopez');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('G', '55667788', 'Alcaldia CCS');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('V', '99887766', 'Ana Rodriguez');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('E', '22334455', 'Luis Fernandez');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('J', '44556677', 'Inversiones ABC');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('V', '33445566', 'Rosa Diaz');
+INSERT INTO cliente (tipo_doc, num_doc, nombre) VALUES ('V', '77889900', 'Pedro Gomez');
 
--- INSERTS INTO FACTURA:
-INSERT INTO cuenta (cliente_id, tipo)
-	VALUES	(1, 'Ahorros'),
-			(2, 'Ahorros'),
-			(2, 'Corriente'),
-			(3, 'Ahorros');
+-- 2. Tabla: producto
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Harina Pan', 1.20, 100, 10);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Arroz 1kg', 0.90, 80, 15);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Pasta 500g', 1.10, 60, 10);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Azucar 1kg', 1.50, 50, 20);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Aceite 1L', 3.50, 40, 5);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Cafe 250g', 2.80, 30, 8);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Leche 1L', 1.80, 25, 10);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Mantequilla', 2.00, 45, 12);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Jabon Baño', 0.75, 120, 30);
+INSERT INTO producto (nom_producto, precio, existencias, minimo) VALUES ('Detergente', 4.50, 15, 10);
 
--- INSERTS INTO PRODUCTO:
-INSERT INTO cuenta (cliente_id, tipo)
-	VALUES	(1, 'Ahorros'),
-			(2, 'Ahorros'),
-			(2, 'Corriente'),
-			(3, 'Ahorros');
+-- 3. Tabla: factura
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (1, 0.16, 'Contado');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (2, 0.16, 'Crédito');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (3, 0.16, 'Contado');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (4, 0.16, 'Contado');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (5, 0.00, 'Crédito');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (6, 0.16, 'Contado');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (7, 0.16, 'Contado');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (8, 0.16, 'Crédito');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (9, 0.16, 'Contado');
+INSERT INTO factura (cliente_id, impuesto, tipo_pago) VALUES (10, 0.16, 'Contado');
 
--- INSERTS INTO DETALLE DE FACTURA:
-INSERT INTO transaccion (cuenta_id, descripcion, deposito) VALUES (1, 'Apertura.', 1000);
-INSERT INTO transaccion (cuenta_id, descripcion, retiro) VALUES (1, 'Cajero automatico.', 200);
-INSERT INTO transaccion (cuenta_id, descripcion, retiro) VALUES (1, 'Compra Supermercado.', 500);
-INSERT INTO transaccion (cuenta_id, descripcion, deposito) VALUES (1, 'Pago Móvil.', 700);
-INSERT INTO transaccion (cuenta_id, descripcion, deposito) VALUES (2, 'Apertura.', 1000);
-INSERT INTO transaccion (cuenta_id, descripcion, retiro) VALUES (2, 'Cajero automatico.', 200);
-INSERT INTO transaccion (cuenta_id, descripcion, deposito) VALUES (3, 'Apertura.', 1000);
-INSERT INTO transaccion (cuenta_id, descripcion, deposito) VALUES (1, 'Pago Movil.', 200);
-
--- SELECT * FROM cliente;
--- SELECT * FROM cuenta;
--- SELECT * FROM transaccion;
+-- 4. Tabla: detalle_factura
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (1, 1, 2, 1.20);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (2, 2, 5, 0.90);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (3, 3, 1, 1.10);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (4, 4, 3, 1.50);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (5, 5, 2, 3.50);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (6, 6, 1, 2.80);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (7, 7, 4, 1.80);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (8, 8, 2, 2.00);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (9, 9, 10, 0.75);
+INSERT INTO detalle_factura (factura_id, producto_id, cantidad, precio) VALUES (10, 10, 1, 4.50);
